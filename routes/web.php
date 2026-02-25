480ea9b640c6a64d8224baacf7547cf9f77dd472
fifanuurunhalizah09 <eljeaje@gmail.com>
2026-02-25 16:03:49 +0700

    prak1_no17

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user/{name?}', function ($name=null) {
+Route::get('/user/{name?}', function ($name='John') {
     return 'Nama saya '.$name;
 });
