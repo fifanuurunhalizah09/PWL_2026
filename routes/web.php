ec93c7fe27211197c1800700e335e3a2a37018f0
fifanuurunhalizah09 <eljeaje@gmail.com>
2026-02-25 15:58:24 +0700

    prak1_no2

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
-use App\Http\Controllers\PageController;
-use App\Http\Controllers\WelcomeController;
+
 use Illuminate\Support\Facades\Route;
 
-Route::get('/article/{id}', [PageController::class,'article']);
 
-
+Route::get('/hello', function () {
+    return 'Hello World';
+});
