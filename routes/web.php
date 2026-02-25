26fba42432ae2d5908931bfa64a7f8290aca6a8e
fifanuurunhalizah09 <eljeaje@gmail.com>
2026-02-25 16:07:29 +0700

    prak2_no6

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
+
+use App\Http\Controllers\PageController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-
-Route::get('/hello', [WelcomeController::class,'hello']);
+Route::get('/index', [PageController::class,'index']);
