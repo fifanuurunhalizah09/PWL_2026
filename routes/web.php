6b4a271c909a019e68958574eba1cb2fb17c4c0f
fifanuurunhalizah09 <eljeaje@gmail.com>
2026-02-25 16:02:59 +0700

    prak1_no14

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/articles/{id}', function ($id) {
-    return "Halaman Artikel dengan ID " . $id;
+Route::get('/user/{name?}', function ($name=null) {
+    return 'Nama saya '.$name;
 });
