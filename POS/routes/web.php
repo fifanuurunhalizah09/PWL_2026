9375ec96edfa9cee82dd99226cc6a9600539cbc0
fifanuurunhalizah09 <eljeaje@gmail.com>
2026-02-26 05:18:26 +0700

    halaman category

--- POS/routes/web.php
+++ POS/routes/web.php
@@ -3,5 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CategoryController;
 
-Route::get('/product', [ProductController::class, 'index']);
+Route::get('/category', [CategoryController::class, 'index']);
