4f2891df5b2a167d808c8de51882329a157f443a
fifanuurunhalizah09 <eljeaje@gmail.com>
2026-02-25 16:02:30 +0700

    prak1_no13

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/posts/{post}/comments/{comment}', function ($postID, $commentId){
-    return 'Pos ke-' .$postID." Komentar ke-: ".$commentId;
+Route::get('/articles/{id}', function ($id) {
+    return "Halaman Artikel dengan ID " . $id;
 });
