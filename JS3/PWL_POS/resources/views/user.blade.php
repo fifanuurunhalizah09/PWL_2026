4fe04e671933761398c50dbdfb23c5afd219421d
fifanuurunhalizah09 <eljeaje@gmail.com>
2026-03-11 13:29:06 +0700

    prak2.4_no1dan2

--- JS3/PWL_POS/resources/views/user.blade.php
+++ JS3/PWL_POS/resources/views/user.blade.php
@@ -4,26 +4,20 @@
         <title>Data User</title>
     </head>
     <body>
-        <h1>Data User</h1>
-        <table border="1" cellpadding="2" cellspacing="0">
-            <tr>
-                <td>Jumlah Pengguna</td>
-                {{-- <th>ID</th>
-                <th>Username</th>
-                <th>Nama</th>
-                <th>ID Level Pengguna</th> --}}
-            </tr>
-            <tr>
-                <td>{{ $data }}</td>
-            </tr>
-            {{-- @foreach ($data as $d)
-            <tr>
-                <td>{{ $data->user_id }}</td>
-                <td>{{ $data->username }}</td>
-                <td>{{ $data->nama }}</td>
-                <td>{{ $data->level_id }}</td>
-            </tr>
-            @endforeach --}}
-        </table>
-    </body>
+    <h1>Data User</h1>
+    <table border="1" cellpadding="2" cellspacing="0">
+        <tr>
+            <td>ID</td>
+            <td>Username</td>
+            <td>Nama</td>
+            <td>ID Level Pengguna</td>
+        </tr>
+        <tr>
+            <td>{{ $data->user_id }}</td>
+            <td>{{ $data->username }}</td>
+            <td>{{ $data->nama }}</td>
+            <td>{{ $data->level_id }}</td>
+        </tr>
+    </table>
+</body>
 </html>
