ad4d6f8ad0c624405ec3e173d513154d37a8769b
fifanuurunhalizah09 <eljeaje@gmail.com>
2026-03-11 13:22:42 +0700

    perbaikan error 2.3

--- JS3/PWL_POS/resources/views/user.blade.php
+++ JS3/PWL_POS/resources/views/user.blade.php
@@ -7,19 +7,23 @@
         <h1>Data User</h1>
         <table border="1" cellpadding="2" cellspacing="0">
             <tr>
-                <th>ID</th>
+                <td>Jumlah Pengguna</td>
+                {{-- <th>ID</th>
                 <th>Username</th>
                 <th>Nama</th>
-                <th>ID Level Pengguna</th>
+                <th>ID Level Pengguna</th> --}}
             </tr>
-            @foreach ($data as $d)
+            <tr>
+                <td>{{ $data }}</td>
+            </tr>
+            {{-- @foreach ($data as $d)
             <tr>
                 <td>{{ $data->user_id }}</td>
                 <td>{{ $data->username }}</td>
                 <td>{{ $data->nama }}</td>
                 <td>{{ $data->level_id }}</td>
             </tr>
-            @endforeach
+            @endforeach --}}
         </table>
     </body>
 </html>
